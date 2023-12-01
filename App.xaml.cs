6ed4d330a56d1342f70becdb6e67c10be3b2fc59
hieubris <ale19@uci.edu>
2023-12-01 11:57:53 -0800

    Initializing password maanager project

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+/* 11-30-2023
+ *
+ * DONE: Began project initialization. Added some style elements to the GUI as an attempt to 
+ * get familiar with XAML.
+ * 
+ * TODO: Create project repo on Github. Continue watching tutorial videos & learning.
+ * Continue playing with GUI elements in order to further familiarize with XAML.
+ * 
+ */
+namespace PasswordManager
+{
+    public partial class App : Application
+    {
+    }
+
+}
